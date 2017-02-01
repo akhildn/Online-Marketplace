b9fc59dff371a314af9c3925e126187c156961e6
anayabu <anayabu@iupui.edu>
2017-02-01 14:28:09 -0500

    Project Restructuring
    -change in implementation of rmi (used naming.lookup, naming.rebind)
    -clean up un-necessary folders and files

---
+++ code/MarketPlace/src/com/iupui/marketplace/client/MarketplaceClient.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iupui.marketplace.client;
+
+/**
+ *
+ * @author Akhil Nayabu
+ */
+
+import java.rmi.Naming;
+import java.rmi.registry.*;
+
+import com.iupui.marketplace.*;
+
+public class MarketplaceClient{
+	
+	public static void main(String args[]){
+	
+		try {
+			
+        /*    Registry registry = LocateRegistry.getRegistry("tesla.cs.iupui.edu",2005);
+            MarketplaceController controller= (MarketplaceController) registry.lookup("controller");
+         */   
+
+
+		MarketplaceController controller= (MarketplaceController) Naming.lookup("//127.0.0.1/MarketPlace");
+	
+		int connectionStatus = controller.connect();
+            if(connectionStatus == 1){
+            	System.out.println("Connected to server....");
+            	System.out.println("Message from server: " + controller.welcomeMessage());
+            }
+		} catch(Exception e){
+			System.out.println("MarketplaceClient Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		
+	}
+}
---
+++ code/MarketPlace/src/com/iupui/marketplace/server/MarketplaceServer.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iupui.marketplace.server;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.*;
+
+import com.iupui.marketplace.*;
+import com.iupui.marketplace.controller.MarketplaceControllerImpl;
+
+public class MarketplaceServer  {
+
+	private static final long serialVersionUID = 1L;
+
+    
+
+	public static void main(String args[]) {
+		try{
+			
+            System.out.println("Creating a Marketplace Server!");
+                       MarketplaceController controller = new MarketplaceControllerImpl();
+            Naming.rebind("//127.0.0.1/MarketPlace", controller);         
+            
+	/*	Registry registry= LocateRegistry.createRegistry(2005);
+            MarketplaceController controller= new MarketplaceControllerImpl();
+            registry.rebind("controller", controller);
+	*/		
+            System.out.println("Marketplace Server Ready!");
+		} catch (Exception e){
+			System.out.println("Exception: " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+}
