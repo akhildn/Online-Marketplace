f842933bbfdc21648b78d98effb7019db085f246
anayabu <anayabu@iupui.edu>
2017-03-31 03:48:15 -0400

    Assignment#3 feedback
    -added comment

--- code/MarketPlace/src/com/iupui/marketplace/server/AuthorizationException.java
+++ code/MarketPlace/src/com/iupui/marketplace/server/AuthorizationException.java
@@ -7,9 +7,11 @@ import java.io.Serializable;
  */
 
 //Ryan: Please include comments in each file.
+//fixed: comments are provided.
 public class AuthorizationException extends RuntimeException implements Serializable {
     private static final long serialVersionUID = 5528415690278423524L;
 
+    // Exception which is displayed when user with invalid premission is trying to access the method
     public AuthorizationException(String methodName, String userType) {
         super("Invalid Authorization - Access Denied to " + methodName + "() function!" +"\n" + userType + ": does not"
                 + " "+"have access to this method.");
